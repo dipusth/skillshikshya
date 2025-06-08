32b2b7751250fac9d2333e7d236feb88544b7837
dipu <deepshrestha365@gmail.com>
2025-06-08 18:41:09 +0545

    layout updated till courses block

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,30 @@ for (let i = 0; i < 2; i++) {
   let clonedList = marqueeList.cloneNode(true); // deep clone with true
   marqueeListWrapper.appendChild(clonedList);   // append as sibling
 }
+
+// owl-carousel
+
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        merge:true,
+        loop:true,
+        margin:32,
+        video:true,
+        lazyLoad:true,
+        autoplay:true,
+        center:true,
+        responsive:{
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1199:{
+                items:4
+            },
+            1440:{
+                items:6
+            }
+        }
+    })
